49e4393694889e0d728817ef4b75a623e1d03caa
tw_akash <akash.verma@trivialworks.com>
2023-10-20 17:55:47 +0530

    jitpack config

--- ShakeTrace/build.gradle.kts
+++ ShakeTrace/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -45,4 +46,20 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+}
+
+afterEvaluate {
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "com.github.theRockAkash"
+                artifactId = "ShakeTrace"
+                version = "1.0.0"
+
+                from(components["release"])
+            }
+        }
+    }
+
 }
